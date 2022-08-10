49c8195332949b030a3c7661b2f31ccf6cd6c5bb
TeoMihuc <37108642+TeoMihuc@users.noreply.github.com>
2022-08-10 09:19:28 +0300

    removed webpack conf

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 20,
-    }
-    return config
-  },
   reactStrictMode: true,
 };
