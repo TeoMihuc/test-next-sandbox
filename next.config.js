f76af7122ff9973767f1a0cc38296d79962ae1fd
TeoMihuc <37108642+TeoMihuc@users.noreply.github.com>
2022-08-10 07:52:47 +0300

    added webpack pooling options

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  watchOptions: {
+    poll: 1000, // Check for changes every second
+  },
+};
