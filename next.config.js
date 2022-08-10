18becef092206c7e88ae5e3f0fba58eba4744a7a
TeoMihuc <37108642+TeoMihuc@users.noreply.github.com>
2022-08-10 11:42:49 +0300

    changed config a bit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 2,
-      aggregateTimeout: 2,
-    }
-    return config
-  },
+  watch: true,
 }
