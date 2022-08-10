ce6c8d8e19a31756a01b0b8083b407289e796a5a
TeoMihuc <37108642+TeoMihuc@users.noreply.github.com>
2022-08-10 10:58:16 +0300

    add webpack here again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 200,
+    },
 }
 
 module.exports = nextConfig
