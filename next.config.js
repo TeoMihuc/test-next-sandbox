2d65312ca402ad43841bb0ef9ab8f676fd2b9aae
TeoMihuc <37108642+TeoMihuc@users.noreply.github.com>
2022-08-10 12:15:04 +0300

    add ignore dir

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
    webpackDevMiddleware: config => {
        config.watchOptions = {
          poll: 1000,
+         ignored: /app/node_modules/,
        }
        return config
   },
